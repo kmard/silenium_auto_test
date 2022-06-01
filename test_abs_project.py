f90a5ec618f37aecefbc717ffebc584c4df5fb56
KM <tsnok777@gmail.com>
2022-06-01 13:06:28 +0300

    Помещение тестового сценария в функцию.

--- test_abs_project.py
+++ test_abs_project.py
@@ -2,6 +2,7 @@
 def test_abs1():
     assert abs(-42) == 42, "Should be absolute value of a number"
 
-if __name__ == '__maine__':
+
+if __name__ == '__main__':
     test_abs1()
     print('All tetst passed!')
