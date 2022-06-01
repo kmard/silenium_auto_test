f9b34e881db53a36d50dd6d3c0105d0a3a5615f5
kmard <34414927+kmard@users.noreply.github.com>
2022-06-01 13:07:36 +0300

    Помещение тестового сценария в функцию.
    Signed-off-by: KM <>
    Signed-off-by: KM <tsnok777@gmail.com>

--- test_abs_project.py
+++ test_abs_project.py
@@ -1,8 +1,13 @@
+import unittest
 
-def test_abs1():
-    assert abs(-42) == 42, "Should be absolute value of a number"
+
+class TestAbs(unittest.TestCase):
+    def test_abs1(self):
+        self.assertEqual(abs(-42), 42, 'Should be absolute value of a number')
+
+    def test_abs2(self):
+        self.assertEqual(abs(-42), -42, 'Should be absolute value of a number')
 
 
 if __name__ == '__main__':
-    test_abs1()
-    print('All tetst passed!')
+    unittest.main()
