54f09cc71a8c29bfce3d0fe1196cbf0ada218814
KM <tsnok777@gmail.com>
2022-06-02 10:10:33 +0300

    run seek element Name
    Signed-off-by: KM <tsnok777@gmail.com>

--- runUnittest.py
+++ runUnittest.py
@@ -1,13 +1,27 @@
 import unittest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import time
+
+def runRegistration(link):
+    with webdriver.Chrome() as browser:
+        browser.get(link)
+
+        # short writing
+        first_name_field = browser.find_element(By.CLASS_NAME, 'first').send_keys('Name1')
+        # long writing
+        # browser.find_element(By.CLASS_NAME, 'first')
+        # first_name_field.click()
+        # first_name_field.send_keys("Name")
+
+        time.sleep(2)
 
 #name mast start with Test
 class TestFourPages(unittest.TestCase):
 
     #name mast start with Test
     def testFirstPage(self):
-        print('testFirstPage')
+        runRegistration('http://suninjuly.github.io/registration1.html')
 
     # name mast start with Test
     def testSecondPage(self):
