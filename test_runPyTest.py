c05f404e3b84d41258a8ce969499db99598de37e
KM <tsnok777@gmail.com>
2022-06-02 11:41:20 +0300

    run PyTest
    Signed-off-by: KM <tsnok777@gmail.com>

---
+++ test_runPyTest.py
@@ -0,0 +1,53 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+#ATTENTION NAME File must begin with test ex. test_runPyTest.py
+
+def runRegistration(link, numberPage):
+    with webdriver.Chrome() as browser:
+        browser.get(link)
+
+        # short writing
+        first_name_field = browser.find_element(By.CLASS_NAME, 'first').send_keys('Name1')
+        # long writing
+        # browser.find_element(By.CLASS_NAME, 'first')
+        # first_name_field.click()
+        # first_name_field.send_keys("Name")
+
+        second_name_field = browser.find_element(By.CLASS_NAME, "second").send_keys('Second2')
+
+        email_field = browser.find_element(By.CLASS_NAME, "third").send_keys('email@domain.com')
+
+        # if numberPage == 1:
+        #     phone_field = browser.find_element(By.CSS_SELECTOR, '[placeholder="Input your phone:"]').send_keys('+7900000000')
+        # else:
+        #     # for next pages
+        #     phone_field = browser.find_element(By.CSS_SELECTOR, '[placeholder="Input your phone:"]').send_keys('+7900000000')
+        #     # time.sleep(100)
+
+        # button registration
+        button = browser.find_element(By.CSS_SELECTOR, "button.btn").click()
+
+        #wait for registration and refresh  welcome_text
+        time.sleep(1)
+
+        #recieve text message
+        welcome_text = browser.find_element(By.TAG_NAME, "h1").text
+
+        assert(welcome_text,'Congratulations! You have successfully registered!','welcome_text texts are different')
+
+        # time.sleep(10)
+
+#name must start with Test
+def testFirstPage():
+    runRegistration('http://suninjuly.github.io/registration1.html',1)
+
+# name must start with Test
+def testSecondPage():
+    runRegistration('http://suninjuly.github.io/registration2.html',2)
+
+
+if __name__ == '__main__':
+    pytest.main()
