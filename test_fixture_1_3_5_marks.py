8e1b6defc7fc97142d5403057be7812efac96bf9
KM <tsnok777@gmail.com>
2022-06-04 11:19:18 +0300

    mark tests @pytest.mark.smoke
    Signed-off-by: KM <tsnok777@gmail.com>

--- test_fixture_1_3_5_marks.py
+++ test_fixture_1_3_5_marks.py
@@ -18,10 +18,12 @@ class TestMainPage1():
 
     @pytest.mark.smoke
     def test_guest_should_see_login_link(self, browser):
+        print('!!!@pytest.mark.smoke')
         browser.get(link)
         browser.find_element(By.CSS_SELECTOR, "#login_link")
 
     @pytest.mark.regression
     def test_guest_should_see_basket_link_on_the_main_page(self, browser):
+        print('!!!@pytest.mark.regression')
         browser.get(link)
         browser.find_element(By.CSS_SELECTOR, ".basket-mini .btn-group > a")
