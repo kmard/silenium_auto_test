5382c28ada9137c0930c0b00f9eb72533044e609
KM <tsnok777@gmail.com>
2022-06-06 11:23:36 +0300

    page as object
    Signed-off-by: KM <tsnok777@gmail.com>

---
+++ PageObject/Test1/test_main_page.py
@@ -0,0 +1,11 @@
+from init import *
+
+def test_guest_can_go_to_login_page(browser):
+
+    link = "http://selenium1py.pythonanywhere.com/"
+    browser.get(link)
+    go_to_login_page(browser)
+
+def go_to_login_page(browser):
+    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
+    login_link.click()
