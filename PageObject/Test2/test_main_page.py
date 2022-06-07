52533f421bfac3efa725196bbaf56ede4c74fa81
KM <tsnok777@gmail.com>
2022-06-07 11:37:58 +0300

    first test page as object moved the attribute search to a separate file
    Signed-off-by: KM <tsnok777@gmail.com>

--- PageObject/Test2/test_main_page.py
+++ PageObject/Test2/test_main_page.py
@@ -5,6 +5,7 @@ from init import *
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
+    # link = 'https://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=midsummer'
 
     page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()                       # открываем страницу
