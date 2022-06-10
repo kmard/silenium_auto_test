428f7a2617ee2af43b92feb81391da96ede46d10
KM <tsnok777@gmail.com>
2022-06-10 10:12:28 +0300

    better run
    Signed-off-by: KM <tsnok777@gmail.com>

--- test_fixture1_3_4.py
+++ test_fixture1_3_4.py
@@ -2,6 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 link = "http://selenium1py.pythonanywhere.com/"
 
+#(venv) PS C:\Projects_Python\silenium_auto_test> pytest -s test_fixture1_3_4.py
+
 
 class TestMainPage1():
 
@@ -12,11 +14,11 @@ class TestMainPage1():
 
     @classmethod
     def teardown_class(self):
-        print("quit browser for test suite 1..")
+        print("\nquit browser for test suite 1..")
         self.browser.quit()
 
     def test_guest_should_see_login_link(self):
-        print('start test link 1')
+        print('\nstart test link 1')
         self.browser.get(link)
         self.browser.find_element(By.CSS_SELECTOR, "#login_link")
 
@@ -29,19 +31,19 @@ class TestMainPage1():
 class TestMainPage2():
 
     def setup_method(self):
-        print("start browser for test 2..")
+        print("\nstart browser for test 2..")
         self.browser = webdriver.Chrome()
 
     def teardown_method(self):
-        print("quit browser for test 2..")
+        print("\nquit browser for test 2..")
         self.browser.quit()
 
     def test_guest_should_see_login_link(self):
-        print('start test link 2')
+        print('\nstart test link 2')
         self.browser.get(link)
         self.browser.find_element(By.CSS_SELECTOR, "#login_link")
 
     def test_guest_should_see_basket_link_on_the_main_page(self):
-        print('start test basket 2')
+        print('\nstart test basket 2')
         self.browser.get(link)
         self.browser.find_element(By.CSS_SELECTOR, ".basket-mini .btn-group > a")
