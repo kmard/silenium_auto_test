eb6035ac0b82c3867f4367805764018308e5a8ed
KM <tsnok777@gmail.com>
2022-06-11 10:31:15 +0300

    filter only @pytest.mark.smoke
    Signed-off-by: KM <tsnok777@gmail.com>

--- test_fixture_1_3_5_marks_part1.py
+++ test_fixture_1_3_5_marks_part1.py
@@ -9,13 +9,22 @@ link = "http://selenium1py.pythonanywhere.com/"
 def browser():
 
     # https://docs.pytest.org/en/latest/how-to/fixtures.html#adding-finalizers-directly
-    # (venv) PS C:\Projects_Python\silenium_auto_test> pytest -s -v test_fixture_1_3_5_marks.py
+    #Чтобы запустить тест с нужной маркировкой, нужно передать в командной строке параметр -m и нужную метку:
+    # (venv) PS C:\Projects_Python\silenium_auto_test> pytest -s -v -m smoke test_fixture_1_3_5_marks_part1.py
     # После завершения теста, который вызывал фикстуру, выполнение фикстуры продолжится со строки,
     # следующей за строкой со словом yield
+    #
     # Для фикстур можно задавать область покрытия фикстур. Допустимые значения:
     # “function”, “class”, “module”, “session”.
     # Соответственно, фикстура будет вызываться один раз для тестового метода, один раз для класса,
     # один раз для модуля или один раз для всех тестов, запущенных в данной сессии.
+    #
+    #При описании фикстуры можно указать дополнительный параметр autouse=True,
+    # который укажет, что фикстуру нужно запустить для каждого теста даже без явного вызова:
+    #Как же регистрировать метки?
+    #Создайте файл pytest.ini в корневой директории вашего тестового проекта и добавьте в файл следующие строки.
+
+    #
     # :return:
     # browser
 
@@ -26,16 +35,22 @@ def browser():
     print("\nquit browser..")
     browser.quit()
 
+@pytest.fixture(autouse=True)
+def prepare_data():
+    print()
+    print("\npreparing some critical data for every test")
+
 
 class TestMainPage1():
 
-
+    @pytest.mark.smoke
     def test_guest_should_see_login_link(self, browser):
         print('\n!!!pytest.mark.smoke')
+        # не передаём как параметр фикстуру prepare_data, но она все равно выполняется
         browser.get(link)
         browser.find_element(By.CSS_SELECTOR, "#login_link")
 
-
+    @pytest.mark.regression
     def test_guest_should_see_basket_link_on_the_main_page(self, browser):
         print('\n!!!pytest.mark.regression')
         browser.get(link)
