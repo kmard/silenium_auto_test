b909d5ed8f858d2c5ad87406295958d21ab60f38
KM <tsnok777@gmail.com>
2022-06-12 07:27:36 +0300

    run with en/rus languages
    Signed-off-by: KM <tsnok777@gmail.com>

---
+++ test_fixture_1_3_6_two_language.py
@@ -0,0 +1,54 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+link = "http://selenium1py.pythonanywhere.com/"
+
+
+@pytest.fixture(scope="function")
+def browser():
+
+    # https://docs.pytest.org/en/latest/how-to/fixtures.html#adding-finalizers-directly
+    # (venv) PS C:\Projects_Python\silenium_auto_test> pytest -s -v test_fixture_1_3_6_two_language.py
+    #Call 4 tests for each language 2 -russian +2 english
+    # После завершения теста, который вызывал фикстуру, выполнение фикстуры продолжится со строки,
+    # следующей за строкой со словом yield
+    #
+    # Для фикстур можно задавать область покрытия фикстур. Допустимые значения:
+    # “function”, “class”, “module”, “session”.
+    # Соответственно, фикстура будет вызываться один раз для тестового метода, один раз для класса,
+    # один раз для модуля или один раз для всех тестов, запущенных в данной сессии.
+    #
+    #При описании фикстуры можно указать дополнительный параметр autouse=True,
+    # который укажет, что фикстуру нужно запустить для каждого теста даже без явного вызова:
+    # :return:
+    # browser
+
+
+    print("\nstart browser for test..")
+    browser = webdriver.Chrome()
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
+
+@pytest.fixture(autouse=True)
+def prepare_data():
+    print()
+    print("\npreparing some critical data for every test")
+
+
+class TestMainPage1():
+
+    @pytest.mark.parametrize('language', ["ru", "en-gb"])
+    def test_guest_should_see_login_link(self, browser,language):
+        print('\n!!!pytest.mark.smoke')
+        # не передаём как параметр фикстуру prepare_data, но она все равно выполняется
+        browser.get(f'{link}{language}/')
+        browser.find_element(By.CSS_SELECTOR, "#login_link")
+
+    @pytest.mark.parametrize('language',['ru','en-gb'])
+    def test_guest_should_see_basket_link_on_the_main_page(self, browser,language):
+        print('\n!!!pytest.mark.regression')
+        nLink = f'{link}{language}/'
+        browser.get(nLink)
+        browser.find_element(By.CSS_SELECTOR, ".basket-mini .btn-group > a")
