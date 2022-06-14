7e63a35e2542946c29449f57451146e978082f09
KM <tsnok777@gmail.com>
2022-06-14 10:09:07 +0300

    find_elements registration
    Signed-off-by: KM <tsnok777@gmail.com>

--- new/webdriver_find_elements_1_6_10.py
+++ new/webdriver_find_elements_1_6_10.py
@@ -3,13 +3,16 @@ from selenium.webdriver.common.by import By
 import time
 
 try:
-    link = "http://suninjuly.github.io/registration1.html"
+    # link = "http://suninjuly.github.io/registration1.html"
+    link ="http://suninjuly.github.io/registration2.html"
     browser = webdriver.Chrome()
     browser.get(link)
 
     elements = browser.find_elements(By.TAG_NAME,'input')
     for element in elements:
         if element.accessible_name == 'Input your first name':
+            element.send_keys(element.accessible_name)
+        elif element.accessible_name == 'Input your name':
             element.send_keys(element.accessible_name)
         elif element.accessible_name == 'Input your last name':
             element.send_keys(element.accessible_name)
@@ -34,7 +37,7 @@ try:
     welcome_text = welcome_text_elt.text
 
     # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
-    assert "Congratulations! You have successfully registered!1" == welcome_text
+    assert "Congratulations! You have successfully registered!" == welcome_text
 
 
 finally:
