13a730fe974ef69da84cdfa9771ff4b1fb8a7d21
KM <tsnok777@gmail.com>
2022-06-19 13:03:48 +0300

    unittest
    Signed-off-by: KM <tsnok777@gmail.com>

--- new/test_3_2_12_unitest.py
+++ new/test_3_2_12_unitest.py
@@ -3,6 +3,7 @@
 
 
 import unittest
+import time
 
 """
 docs https://docs.python.org/3/library/unittest.html
@@ -10,10 +11,11 @@ docs https://docs.python.org/3/library/unittest.html
 class TestAbs(unittest.TestCase):
     def test_abs1(self):
         self.assertEqual(abs(-42), 42, "Should be absolute value of a number")
+        time.sleep(10)
 
     def test_abs2(self):
-        self.assertEqual(abs(-42), -42, "Should be absolute value of a number")
-
+        self.assertEqual(abs(-42), 42, "Should be absolute value of a number")
+        time.sleep(10)
 
 if __name__ == "__main__":
     unittest.main()
---
+++ new/test_3_2_13_unitest.py
@@ -0,0 +1,103 @@
+
+# python test_3_2_13_unitest.py
+
+
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+"""
+docs https://docs.python.org/3/library/unittest.html
+"""
+class TestPage(unittest.TestCase):
+
+    def test_Page1(self):
+
+
+            link = "http://suninjuly.github.io/registration1.html"
+
+            browser = webdriver.Chrome()
+            browser.get(link)
+
+            elements = browser.find_elements(By.TAG_NAME, 'input')
+            self.__fill_elements(elements)
+
+
+            # Отправляем заполненную форму
+            button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+            button.click()
+
+            # Проверяем, что смогли зарегистрироваться
+            # ждем загрузки страницы
+            time.sleep(1)
+
+            # находим элемент, содержащий текст
+            welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
+            # записываем в переменную welcome_text текст из элемента welcome_text_elt
+            welcome_text = welcome_text_elt.text
+
+            # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+            self.assertEqual("Congratulations! You have successfully registered!", welcome_text,'Registration failed registration1')
+
+            # закрываем браузер после всех манипуляций
+            browser.quit()
+
+    def test_Page2(self):
+
+
+            # link = "http://suninjuly.github.io/registration1.html"
+            link = "http://suninjuly.github.io/registration2.html"
+            browser = webdriver.Chrome()
+            browser.get(link)
+
+            elements = browser.find_elements(By.TAG_NAME, 'input')
+            self.__fill_elements(elements)
+
+            # Отправляем заполненную форму
+            button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+            button.click()
+
+            # Проверяем, что смогли зарегистрироваться
+            # ждем загрузки страницы
+            time.sleep(1)
+
+            # находим элемент, содержащий текст
+            welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
+            # записываем в переменную welcome_text текст из элемента welcome_text_elt
+            welcome_text = welcome_text_elt.text
+
+            # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+            self.assertEqual("Congratulations! You have successfully registered!", welcome_text,'Registration failed registration2')
+
+            # закрываем браузер после всех манипуляций
+            browser.quit()
+
+
+    def __fill_elements(self, elements):
+        for element in elements:
+            if element.accessible_name == 'Input your first name':
+                element.send_keys(element.accessible_name)
+            elif element.accessible_name == 'Input your name':
+                element.send_keys(element.accessible_name)
+            elif element.accessible_name == 'Input your last name':
+                element.send_keys(element.accessible_name)
+            elif element.accessible_name == 'Input your email':
+                element.send_keys(element.accessible_name)
+            elif element.accessible_name == 'Input your phone:':
+                element.send_keys(element.accessible_name)
+            elif element.accessible_name == 'Input your address:':
+                element.send_keys(element.accessible_name)
+
+
+if __name__ == "__main__":
+    unittest.main()
+
+
+
+
+# Импортировать unittest в файл: import unittest
+# Создать класс, который должен наследоваться от класса TestCase: class TestPage(unittest.TestCase):
+# Превратить тестовые функции в методы, добавив ссылку на экземпляр класса self в качестве первого аргумента функции: def test_Page1(self):
+# Изменить assert на self.assertEqual()
+# Заменить строку запуска программы на unittest.main()
