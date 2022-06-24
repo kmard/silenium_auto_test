f1d6728221de834e1c0e20fe260f7263573a0f27
KM <tsnok777@gmail.com>
2022-06-24 11:36:07 +0300

    fixture parametrize
    Signed-off-by: KM <tsnok777@gmail.com>

---
+++ new/test_3_6_9_fixture_parametrize.py
@@ -0,0 +1,9 @@
+
+# pytest --language=es test_3_6_9_fixture_parametrize.py
+import time
+
+def test_find_add_to_card_btn(browser):
+    browser.get('http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/')
+    time.sleep(10)
+    assert browser.find_element_by_class_name('btn-add-to-basket').is_displayed(), \
+        'Кнопка добавления товара в корзину отсутсвует'
