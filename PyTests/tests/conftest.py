9d1d3db5175806d3888aa3a60f3a5933621be79c
KM <tsnok777@gmail.com>
2022-06-27 20:35:31 +0300

    Pytest conftest fixture module
    Signed-off-by: KM <tsnok777@gmail.com>

--- PyTests/tests/conftest.py
+++ PyTests/tests/conftest.py
@@ -6,3 +6,10 @@ def set_up():
     print('\nConnect ')
     yield
     print('\nDisconnect ')
+
+# cls,function
+@pytest.fixture(scope='module')
+def some():
+    print('\nBEGIN')
+    yield
+    print('END ')
--- PyTests/tests/test_mail.py
+++ PyTests/tests/test_mail.py
@@ -2,14 +2,14 @@
 #pytest -s -v test_mail.py
 
 
-def test_sebding_mail_1(set_up):
+def test_sebding_mail_1(set_up, some):
     print('letter sended_1')
+
 
 def test_sebding_mail_2(set_up):
     print('letter sended_2')
 
-def test_sebding_mail_3(set_up):
+
+def test_sebding_mail_3(set_up, some):
     # 2/0
     print('letter sended_3')
-
-
