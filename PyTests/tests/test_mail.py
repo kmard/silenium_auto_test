86cd73c79248a3cc81a30f9ccfb1ea9714c49e92
KM <tsnok777@gmail.com>
2022-06-27 19:49:58 +0300

    Pytest yield
    Signed-off-by: KM <tsnok777@gmail.com>

--- PyTests/tests/test_mail.py
+++ PyTests/tests/test_mail.py
@@ -4,10 +4,18 @@ import pytest
 
 @pytest.fixture()
 def set_up():
-    print('\nlogin is complete')
+    print('\nConnect')
+    yield
+    print('\nDisconnect')
 
 def test_sebding_mail_1(set_up):
     print('letter sended_1')
 
 def test_sebding_mail_2(set_up):
     print('letter sended_2')
+
+def test_sebding_mail_3(set_up):
+    # 2/0
+    print('letter sended_3')
+
+
